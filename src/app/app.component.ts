157e2004c40f6adb7752d4b853456461b655fe95
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-07-02 22:38:55 +0200

    impl imprint & router to change from main content to imprint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { NavbarComponent } from './main-content/landingPage/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MainContentComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, MainContentComponent, FooterComponent, ImprintComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
